3370ed42b5b50666ed4d2b30fccdaa640fd2085b
Jeffrey Quesnelle <jquesnelle@gmail.com>
2014-02-25 22:45:42 -0500

    auto setup of empty db

--- penguicontrax.py
+++ penguicontrax.py
@@ -5,7 +5,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.openid import OpenID
 from flask_oauth import OAuth
 from penguicontrax_constants import penguicontrax_constants
-import os
+import os, sqlite3, import2013schedule
 
 app = Flask(__name__)
 app.secret_key = penguicontrax_constants.SESSION_SECRET_KEY
@@ -213,4 +213,14 @@ def oauth_authorized(resp):
     return redirect(next_url)
 
 if __name__ == '__main__':
+    if not os.path.isfile(penguicontrax_constants.DATABASE_FILE):
+        with app.open_resource('schema.sql', mode='r') as f:
+            with sqlite3.connect(penguicontrax_constants.DATABASE_FILE) as sqlitedb:
+                try:
+                    sqlitedb.cursor().executescript(f.read())
+                    sqlitedb.commit()
+                except:
+                    pass
+                # GET RID OF THIS LATER
+                import2013schedule.import_old()
     app.run(host='0.0.0.0', debug=True)
