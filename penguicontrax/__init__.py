7878f3d647ca3a3e25526b4db6c8b10cf1015703
Jeffrey Quesnelle <jquesnelle@gmail.com>
2014-02-28 18:01:50 -0500

    added /report as simple xml report for external apps

--- penguicontrax/__init__.py
+++ penguicontrax/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.cache import Cache
 app = Flask(__name__)
 db =  SQLAlchemy(app);      
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
-from flask import render_template, g
+from flask import render_template, g, url_for, redirect, Response
 from submission import Submission, Tag
 from user import Login
 import os, sqlite3, import2013schedule
 from constants import constants
-
+import xml.etree.ElementTree as ET
+import datetime
 
 def init():
     app.secret_key = constants.SESSION_SECRET_KEY
@@ -29,7 +32,23 @@ def index():
     submissions = Submission.query.order_by('id').all() if g.user is not None and g.user.staff == True else Submission.query.filter(Submission.followUpState != 3).order_by('id')
     tags = [tag.name for tag in Tag.query.all()]
     return render_template('index.html', tags=tags, submissions=submissions, user=g.user)
+  
+def dump_table_xml(elements, table, parent_node, collection_name, element_name):
+    collection = ET.SubElement(parent_node, collection_name)
+    for element in elements:
+        element_node = ET.SubElement(collection, element_name)
+        element_dict = dict((col, getattr(element, col)) for col in table.columns.keys())
+        for key, value in element_dict.iteritems():
+            ET.SubElement(element_node, str(key)).text = unicode(value)
+    return collection
        
-    
-
-    
+@app.route('/report')
+@cache.cached(timeout=900)
+def report():
+    root = ET.Element('penguicontrax')
+    ET.SubElement(root, 'generated').text = str(datetime.datetime.now())
+    dump_table_xml(Submission.query.all(), Submission.__table__, root, 'submissions', 'submission')
+    dump_table_xml(Tag.query.all(), Tag.__table__, root, 'tags', 'tag')
+    from submission import tags
+    dump_table_xml(db.session.query(tags).all(), tags, root, 'tag-assignments', 'tag-assignment')
+    return Response(ET.tostring(root, encoding='utf-8'), mimetype='text/xml')
