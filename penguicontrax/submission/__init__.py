0a9a470ea0c7d0ebe317b67f41eab5c88e88d19b
Jeffrey Quesnelle <jquesnelle@gmail.com>
2014-02-28 05:20:59 -0500

    tag editing now works

--- penguicontrax/submission/__init__.py
+++ penguicontrax/submission/__init__.py
@@ -2,7 +2,7 @@ from flask import g, request, session, render_template, redirect
 from .. import app, db
 
 tags = db.Table('tags', 
-                db.Column('submission_id', db.Integer, db.ForeignKey('submission.id')), 
+                db.Column('submission_id', db.Integer, db.ForeignKey('submission.id', ondelete='CASCADE')), 
                 db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')))
     
 class Tag(db.Model):
@@ -24,11 +24,11 @@ class Submission(db.Model):
     firstname = db.Column(db.String())
     lastname = db.Column(db.String())
     trackId = db.Column(db.Integer())
-    duration = db.Column(db.Boolean()) 
-    setupTime = db.Column(db.Boolean()) 
-    repetition = db.Column(db.Boolean()) 
+    duration = db.Column(db.Integer()) 
+    setupTime = db.Column(db.Integer()) 
+    repetition = db.Column(db.Integer()) 
     followUpState = db.Column(db.Integer()) # 0 = submitted, 1 = followed up, 2 = accepted, 3 = rejected
-    tags = db.relationship('Tag', secondary=tags, backref=db.backref('submissions', lazy='dynamic'))
+    tags = db.relationship('Tag', secondary=tags, backref=db.backref('submissions', passive_deletes=True))
         
     def __init(self):
         pass
@@ -53,29 +53,36 @@ def event_form():
     tags = [tag.name for tag in Tag.query.all()]
     if request.method == 'GET':
         eventid = request.args.get('id',None)
+        event_tags = []
         if eventid is not None:
             event = Submission.query.filter_by(id=eventid).first()
+            event_tags = [tag.name for tag in event.tags]
         else:
             event = None
-    return render_template('form.html', tags=tags, event=event, user=g.user)
+    return render_template('form.html', tags=tags, event=event, user=g.user, event_tags=event_tags)
 
 @app.route('/submitevent', methods=['POST'])
 def submitevent():
-    eventid = request.form['eventid']
+    eventid = request.form['eventid'] if 'eventid' in request.form else None
     if eventid is not None:
         submission = Submission.query.filter_by(id=eventid).first()
-    if submission is None:
+    else:
         submission = Submission()
+        submission.followUpState = 0
     submission.email = request.form['email']
     submission.title = request.form['title']
     submission.description = request.form['description']
     submission.duration = request.form['duration']
-    submission.setupTime = request.form['setuptime'] if request.form['setuptime'] is not None else 1 
+    submission.setupTime = request.form['setuptime'] if 'setuptime' in request.form else 1 
     submission.repetition = request.form['repetition']
     submission.comments = request.form['comments']
     submission.firstname = request.form['firstname']
     submission.lastname = request.form['lastname']
-    submission.followUpState = request.form['followupstate'] if request.form['followupstate'] is not None else 0
+    if 'followupstate' in request.form:
+        submission.followUpState = request.form['followupstate']
+    submission.tags = []
+    for tag in request.form.getlist('tagbtn'):
+        submission.tags.append(Tag.query.filter_by(name=tag).first())
     db.session.add(submission)
     db.session.commit()
     return redirect('/')
