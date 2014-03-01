d5fe1e48cbea1e309de43589347a482a66664f16
Jeffrey Quesnelle <jquesnelle@gmail.com>
2014-03-01 13:58:57 -0500

    adds /getevent service endpoint

--- penguicontrax/__init__.py
+++ penguicontrax/__init__.py
@@ -1,16 +1,30 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.cache import Cache
+import xml.etree.ElementTree as ET
+import json
 app = Flask(__name__)
 db =  SQLAlchemy(app);      
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+def dump_table_xml(elements, table, parent_node, collection_name, element_name):
+    collection = ET.SubElement(parent_node, collection_name)
+    for element in elements:
+        element_node = ET.SubElement(collection, element_name)
+        element_dict = dict((col, getattr(element, col)) for col in table.columns.keys())
+        for key, value in element_dict.iteritems():
+            ET.SubElement(element_node, str(key)).text = unicode(value)
+    return collection
+
+def dump_table_json(elements, table):
+    all = [(dict((col, getattr(element, col)) for col in table.columns.keys())) for element in elements]
+    return json.dumps(all)
 
 from flask import render_template, g, url_for, redirect, Response
 from submission import Submission, Tag
 from user import Login
 import os, sqlite3, import2013schedule
 from constants import constants
-import xml.etree.ElementTree as ET
 import datetime
 
 def init():
@@ -32,16 +46,8 @@ def index():
     submissions = Submission.query.order_by('id').all() if g.user is not None and g.user.staff == True else Submission.query.filter(Submission.followUpState != 3).order_by('id')
     tags = [tag.name for tag in Tag.query.all()]
     return render_template('index.html', tags=tags, submissions=submissions, user=g.user)
-  
-def dump_table_xml(elements, table, parent_node, collection_name, element_name):
-    collection = ET.SubElement(parent_node, collection_name)
-    for element in elements:
-        element_node = ET.SubElement(collection, element_name)
-        element_dict = dict((col, getattr(element, col)) for col in table.columns.keys())
-        for key, value in element_dict.iteritems():
-            ET.SubElement(element_node, str(key)).text = unicode(value)
-    return collection
-       
+
+        
 @app.route('/report')
 @cache.cached(timeout=900)
 def report():
