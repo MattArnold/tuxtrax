7c60af37ae61a1b2e00ec3a4d6332c0b7fc1fcff
Stan Carrico <shcarrico@gmail.com>
2014-03-01 15:53:21 -0500

    grunt,bower,npm, less integration

---
+++ penguicontrax/Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          paths: ["static/ptrax.less"]
+        },
+        files: {
+          "static/ptrax.css": "static/ptrax.less"
+        }
+      }
+    },
+    watch: {
+      files: "static/ptrax.less",
+      tasks: ["less"]
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+};
