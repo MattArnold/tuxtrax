923e442d05a2a961b9e254d3c7ba679ecbfb7ba8
Chad Crabtree <flaxeater@gmail.com>
2014-03-01 20:04:24 -0500

    adding in the user search api call

--- penguicontrax/api/__init__.py
+++ penguicontrax/api/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask, request
 from flask.ext.restful import Resource, Api
-from .. import app
+from penguicontrax import app
 
 #api modules
 import events
+import users
 
 api = Api(app)
 #define routes
+
+#events
 api.add_resource(events.EventAPI,'/api/event/<string:event_id>')
 api.add_resource(events.EventsAPI,'/api/events')
 
-
+#users
+api.add_resource(users.SearchUserAPI,'/api/search_users/<string:search_string>')
