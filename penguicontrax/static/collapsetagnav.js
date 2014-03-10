88086b01d0956acc38d304ea0c8d91517c10c180
Matt Arnold <matt.mattarn@gmail.com>
2014-03-10 16:06:58 -0400

    Separated nav hide and show into its own js file.

---
+++ penguicontrax/static/collapsetagnav.js
@@ -0,0 +1,15 @@
+    //Handle collapsing and expanding the tag navigation panel
+$(document).ready(function () {
+    $('#hidenav').click(function () {
+        $('.tagsinput').css('display', 'none');
+        $('#shownav').css('display', 'block');
+        $('#report').removeClass('col-md-10');
+        $('#report').addClass('col-md-12');
+    });
+    $('#shownavbtn').click(function () {
+        $('.tagsinput').css('display', 'block');
+        $('#shownav').css('display', 'none');
+        $('#report').removeClass('col-md-12');
+        $('#report').addClass('col-md-10');
+    });
+});
