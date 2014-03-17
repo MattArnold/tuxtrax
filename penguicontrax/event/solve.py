2b85718191b9074810f416373371a43baac656f2
Jeffrey Quesnelle <jquesnelle@gmail.com>
2014-03-17 17:58:24 -0400

    ecttd doesn't cause schedule conflicts but still isn't assigning rooms correctly

--- penguicontrax/event/solve.py
+++ penguicontrax/event/solve.py
@@ -19,9 +19,11 @@ def solve_convention(convention, type = SolveTypes.TTD, write_files = False):
         
         yield 'Creating lists of presenters</br>'
         event_presenters= []
+        presenters_upperbound = 0
         for event in total_events:
             user_ids = [user.id for user in event.userPresenters]
             person_ids = [person.id for person in event.personPresenters]
+            presenters_upperbound += len(user_ids) + len(person_ids)
             event_presenters.append((user_ids, person_ids))
         combined_presenters_set = set()
         for event_presenter_lists in event_presenters:
@@ -60,16 +62,23 @@ def solve_convention(convention, type = SolveTypes.TTD, write_files = False):
         
         prob = LpProblem('Convention Time Table Optimization Problem', LpMinimize)
         
-        num_vars = len(P)*len(T)*len(H)
-        yield 'Creating %d scheduling variables<br/>' % num_vars
-        f = LpVariable.dicts("f", range(num_vars), cat="Integer")
+        num_vars_f = len(P)*len(T)*len(H)
+        num_vars_g = len(T)*len(H)*len(R) if type == SolveTypes.ECTTD else 0
+        yield 'Creating %d scheduling variables<br/>' % (num_vars_f + num_vars_g)
+        f = LpVariable.dicts('f', range(num_vars_f), cat='Integer')
         for k in f.viewkeys():
             f[k].lowBound = 0
             f[k].upBound = 1
         index = lambda i,j,h: (i*len(T)*len(H) + j*len(H) + h)
+        if type == SolveTypes.ECTTD:
+            g = LpVariable.dicts('g', range(num_vars_g), cat='Integer')
+            for k in g.viewkeys():
+                g[k].lowBound = 0
+                g[k].upBound = 1
+            indexg = lambda j,h,r: (j*len(H)*len(R) + h*len(R) + r)
     
         yield 'Creating objective function<br/>'
-        prob += lpSum([f[x] for x in range(num_vars)])
+        prob += lpSum(([f[x] for x in range(num_vars_f)].extend([g[x] for x in range(num_vars_g)])) if type == SolveTypes.ECTTD else ([f[x] for x in range(num_vars_f)]))
         
         yield 'Creating constraint that each presenter and presentation must be available<br/>'
         #the presenter and talk are both available to be scheduled at hour
@@ -114,6 +123,14 @@ def solve_convention(convention, type = SolveTypes.TTD, write_files = False):
                     
         if type == SolveTypes.ECTTD:
             yield 'Creating constraint that no room has more than one event per timeslot<br/>'
+            for j in range(len(T)):
+                for h in range(len(H)):
+                    room_sum = lpSum([g[indexg(j,h,r)] for r in range(len(R))])
+                    presenter_sum = lpSum([f[index(i,j,h)] for i in range(len(P))])
+                    prob += room_sum <= presenter_sum 
+                    prob += presenter_sum <= presenters_upperbound*room_sum
+                    prob += room_sum <= 1
+                        
                 
         if write_files == True:
             yield 'Writing linear programming files<br/>'    
@@ -137,13 +154,17 @@ def solve_convention(convention, type = SolveTypes.TTD, write_files = False):
                     scheduled = False
                     room = None
                     event = total_events[j]
-                    for i in range(len(P)):
-                        if type != SolveTypes.ECTTD:
+                    if type != SolveTypes.ECTTD:
+                        for i in range(len(P)):
                             if f[index(i,j,h)].varValue == 1:
                                 scheduled = True
                                 break
-                        else:
-                            pass
+                    else:
+                        for r in range(len(R)):
+                            if g[indexg(j,h,r)].varValue == 1:
+                                scheduled = True
+                                room = R[r]
+                                break
                     if scheduled == True:
                         timeslot = Timeslot.query.filter_by(id=H[h]).first()
                         event.start_dt = timeslot.start_dt
