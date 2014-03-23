090ea8607e3cb84633b787455890311995d88ac2
Walter Huf <hufman@gmail.com>
2014-03-23 16:44:41 -0500

    Adds a user list+search API endpoint

--- penguicontrax/api/__init__.py
+++ penguicontrax/api/__init__.py
@@ -31,10 +31,16 @@ def json_date(data, code, headers=None):
     return resp
 
 #submissions
-##api.add_resource(submissions.SubmissionAPI,'/api/submission/<string:submission_id>/rsvp')
-api.add_resource(submissions.SubmissionAPI, '/api/submission/<string:submission_id>',
+##api.add_resource(submissions.SubmissionAPI,
+##                 '/api/submission/<string:submission_id>/rsvp')
+api.add_resource(submissions.SubmissionAPI,
+                 '/api/submission/<string:submission_id>',
                  '/api/submission/<string:submission_id>/<string:noun>')
-api.add_resource(submissions.SubmissionsAPI, '/api/submissions')
+api.add_resource(submissions.SubmissionsAPI,
+                 '/api/submissions')
 
 #users
-api.add_resource(users.SearchUserAPI, '/api/search_users/<string:search_string>')
+api.add_resource(users.SearchUserAPI,
+                 '/api/search_users/<string:search_string>')
+api.add_resource(users.UsersAPI,
+                 '/api/users')
--- penguicontrax/api/users.py
+++ penguicontrax/api/users.py
@@ -1,10 +1,10 @@
 #flask libs
 from flask import Flask, request
-from flask.ext.restful import Resource, Api
+from flask.ext.restful import Resource, Api, reqparse
 from sqlalchemy import or_
 
 #global libs
-from sys import exit;
+from sys import exit
 import sys
 import os
 
@@ -15,19 +15,46 @@ from functions import return_null_if_not_logged_in
 ##User = user.User
 from penguicontrax.user import User
 
+
+class UsersAPI(Resource):
+    @return_null_if_not_logged_in
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('q', type=str)
+        args = parser.parse_args()
+
+        output = User.query
+        if args['q']:
+            search_string = '%' + args['q'] + '%'
+            output = User.query.filter(
+                or_(
+                    User.name.like(search_string),
+                    User.email.like(search_string),
+                    User.account_name.like(search_string)
+                    )
+                )
+        output = dump_table(output, User.__table__)
+        fields = ['id', 'name', 'email']
+        return [
+            dict([(name, element[name]) for name in fields])
+            for element in output
+        ]
+
+
 class SearchUserAPI(Resource):
     @return_null_if_not_logged_in
-    def get(self,search_string):
-        search_string = '%'+search_string+'%'
+    def get(self, search_string):
+        search_string = '%' + search_string + '%'
         output = User.query.filter(
             or_(
                 User.name.like(search_string),
                 User.email.like(search_string),
-                User.account_name.like(search_string),
+                User.account_name.like(search_string)
                 )
             )
         output = dump_table(output, User.__table__)
-        filters = ['id','name','email']
-        return [dict([(name,element[name]) for name in filters])
-                for element in output 
-            ]
+        fields = ['id', 'name', 'email']
+        return [
+            dict([(name, element[name]) for name in fields])
+            for element in output
+        ]
