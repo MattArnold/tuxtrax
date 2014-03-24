4bada240fb1b60f70a574ddcde02a0b3846bef0e
Walter Huf <hufman@gmail.com>
2014-03-23 19:16:57 -0500

    Adds some documentation about the user api
    Also some minor pep8 fixes

--- penguicontrax/api/users.py
+++ penguicontrax/api/users.py
@@ -19,6 +19,9 @@ from penguicontrax.user import User
 class UsersAPI(Resource):
     @return_null_if_not_logged_in
     def get(self):
+        """ Returns a list of objects to represent users in the database
+            Pass a ?q=query to conduct a search by name and email
+        """
         parser = reqparse.RequestParser()
         parser.add_argument('q', type=str)
         args = parser.parse_args()
@@ -34,6 +37,7 @@ class UsersAPI(Resource):
                     )
                 )
         output = dump_table(output, User.__table__)
+        # fields to show in search results
         fields = ['id', 'name', 'email']
         return [
             dict([(name, element[name]) for name in fields])
@@ -43,12 +47,23 @@ class UsersAPI(Resource):
 
 class UserAPI(Resource):
     def get(self, id):
+        """ Returns information about the given user by id
+            Reveals different information depending on the logged-in user
+                Whether the logged-in user is the same as the given user
+                Whether the session has a user
+        """
         found = User.query.get(id)
         if found:
-            self_fields = ['id', 'name', 'staff', 'email', 'points', 'image_large', 'image_small', 'public_rsvps', 'rsvped_to', 'superuser', 'creation_ip']
-            loggedin_fields = ['id', 'name', 'points', 'image_large', 'image_small', 'public_rsvps', 'rsvped_to']
+            # fields revealed based on login status
+            self_fields = ['id', 'name', 'staff', 'email', 'points',
+                           'image_large', 'image_small',
+                           'public_rsvps', 'rsvped_to', 'superuser',
+                           'creation_ip']
+            loggedin_fields = ['id', 'name', 'points',
+                               'image_large', 'image_small',
+                               'public_rsvps', 'rsvped_to']
             anon_fields = ['id', 'name', 'image_large', 'image_small']
-            if g.user == None:
+            if g.user is None:
                 fields = anon_fields
             elif g.user.id == id:
                 fields = self_fields
@@ -57,12 +72,18 @@ class UserAPI(Resource):
                 if not found.public_rsvps:
                     fields.remove('rsvped_to')
             return dict([(name, getattr(found, name)) for name in fields])
+
     def put(self, id):
+        """ Updates specific information about the given user
+            Currently requires that the given user is currently logged-in
+            Also only allows specific fields to be changed
+            The updated information should be sent as form-encoded data
+        """
         found = User.query.get(id)
         if g.user:
             if found:
                 if g.user.id == id:
-                    fields = ['name', 'email']
+                    fields = ['name', 'email']  # fields allowed to change
                     parser = reqparse.RequestParser()
                     for field in fields:
                         parser.add_argument(field, type=str)
