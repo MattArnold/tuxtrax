a8e604c32d1c41cd41e73dfc2dde6688e933f127
Jeffrey Quesnelle <jquesnelle@gmail.com>
2014-03-26 01:53:35 -0400

    adds soci source and script to build full modeler

---
+++ modeler/makemodeler.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd modeler/soci-3.2.2
+mkdir build
+cd build
+cmake -G "Unix Makefiles" -DWITH_BOOST=OFF ../
+make
+cd ../../
+make
---
+++ modeler/runmodeler.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:soci-3.2.2/build/lib
+bin/modeler $0 $1 $2 $3 $3 $4 $5 $6 $7 $7 $9
