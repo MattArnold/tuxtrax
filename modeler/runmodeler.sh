bf5bb26b4240adb9f233d5a92e27ce2190a0369c
Jeffrey Quesnelle <jquesnelle@gmail.com>
2014-03-26 01:58:22 -0400

    runmodeler.sh can run anywhere

--- modeler/runmodeler.sh
+++ modeler/runmodeler.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:soci-3.2.2/build/lib
-bin/modeler $0 $1 $2 $3 $3 $4 $5 $6 $7 $7 $9
+BASEDIR=$(dirname $0)
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BASEDIR/soci-3.2.2/build/lib
+$BASEDIR/bin/modeler $1 $2 $3 $3 $4 $5 $6 $7 $7 $9
