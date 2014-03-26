d854b6064aedc15d3d609b408f54d6c13473e8e5
Jeffrey Quesnelle <jquesnelle@gmail.com>
2014-03-26 02:39:09 -0400

    makemodeler.sh can run from anywhere

--- modeler/makemodeler.sh
+++ modeler/makemodeler.sh
@@ -1,9 +1,11 @@
 #!/bin/bash 
 
-cd modeler/soci-3.2.2
+BASEDIR=$(dirname $0)
+cd $BASEDIR/soci-3.2.2
 mkdir build
 cd build
 cmake -G "Unix Makefiles" -DWITH_BOOST=OFF ../
 make
 cd ../../
 make
+cd ../
