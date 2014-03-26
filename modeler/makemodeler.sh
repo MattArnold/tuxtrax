6d9d9e0f7a870a9cf017bf17e64401371fc534b9
Jeffrey Quesnelle <jquesnelle@gmail.com>
2014-03-26 05:41:41 -0400

    adds sqlite3 to modeler builder

--- modeler/makemodeler.sh
+++ modeler/makemodeler.sh
@@ -34,6 +34,18 @@ then
 	tar xvfz $TMPDIR/soci-3.2.2.tar.gz -C $BASEDIR
 fi
 
+# Check for sqlite3 file
+if [ ! -f "$TMPDIR/sqlite-autoconf-3080401.tar.gz" ]
+then
+	curl http://sqlite.org/2014/sqlite-autoconf-3080401.tar.gz > $TMPDIR/sqlite-autoconf-3080401.tar.gz
+fi
+
+# Check for sqlite3 source
+if [ ! -d "$BASEDIR/sqlite-autoconf-3080401" ]
+then
+	tar xvfz $TMPDIR/sqlite-autoconf-3080401.tar.gz -C $BASEDIR
+fi
+
 # Make cmake
 cd $BASEDIR/cmake-2.8.12.2
 if [ ! -d "$BASEDIR/cmake-2.8.12.2/Bootstrap.cmk" ]
@@ -42,12 +54,20 @@ then
 fi
 make
 
+# Make sqlite3
+cd $BASEDIR/sqlite-autoconf-3080401
+if [ ! -f "$BASEDIR/sqlite-autoconf-3080401/Makefile" ]
+then
+	./configure --disable-static CFLAGS="-g -O2 -DSQLITE_ENABLE_FTS3=1 -DSQLITE_ENABLE_COLUMN_METADATA=1 -DSQLITE_ENABLE_UNLOCK_NOTIFY=1 -DSQLITE_SECURE_DELETE=1"
+fi
+make
+
 # Make soci
 cd $BASEDIR/soci-3.2.2
 mkdir build
 cd build
 export PATH=$PATH:$BASEDIR/cmake-2.8.12.2/bin
-cmake -G "Unix Makefiles" -DWITH_BOOST=OFF ../
+cmake -G "Unix Makefiles" -DWITH_BOOST=OFF -DSQLITE3_FOUND=ON -DSQLITE3_LIBRARY=$BASEDIR/sqlite-autoconf-3080401/.libs/libsqlite3.so -DSQLITE3_LIBRARIES=$BASEDIR/sqlite-autoconf-3080401/.libs/libsqlite3.so -DSQLITE3_INCLUDE_DIR=$BASEDIR/sqlite-autoconf-3080401 ../
 make
 cd ../../
 
