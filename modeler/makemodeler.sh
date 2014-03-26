78515733123b09876965042d9239071f09c74a0f
Jeffrey Quesnelle <jquesnelle@gmail.com>
2014-03-26 03:22:42 -0400

    removes soci and cmake source, will wget

--- modeler/makemodeler.sh
+++ modeler/makemodeler.sh
@@ -1,9 +1,14 @@
 #!/bin/bash 
 
-BASEDIR=$(dirname $0)
+FILE=$(readlink -f $0)
+BASEDIR=`dirname $FILE`
+cd $BASEDIR/cmake-2.8.12.2
+./bootstrap
+make
 cd $BASEDIR/soci-3.2.2
 mkdir build
 cd build
+export PATH=$PATH:$BASEDIR/cmake-2.8.12.2/bin
 cmake -G "Unix Makefiles" -DWITH_BOOST=OFF ../
 make
 cd ../../
