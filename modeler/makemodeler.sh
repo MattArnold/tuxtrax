5aa5cc040ef04e556c602d00dfad67fc84327ff7
Jeffrey Quesnelle <jquesnelle@gmail.com>
2014-03-26 03:50:45 -0400

    makemodeler.sh that downloads soci and cmake

--- modeler/makemodeler.sh
+++ modeler/makemodeler.sh
@@ -2,9 +2,47 @@
 
 FILE=$(readlink -f $0)
 BASEDIR=`dirname $FILE`
+TMPDIR=$BASEDIR/../tmp
+
+# Make temp directory
+if [ ! -d "$TMPDIR" ]
+then
+	mkdir $TMPDIR
+fi
+
+# Check for cmake file
+if [ ! -f "$TMPDIR/cmake-2.8.12.2.tar.gz" ]
+then
+	wget -P $TMPDIR http://www.cmake.org/files/v2.8/cmake-2.8.12.2.tar.gz 
+fi
+
+# Check for cmake source
+if [ ! -d "$BASEDIR/cmake-2.8.12.2" ]
+then
+	tar xvfz $TMPDIR/cmake-2.8.12.2.tar.gz -C $BASEDIR
+fi
+
+# Check for soci file
+if [ ! -f "$TMPDIR/soci-3.2.2.tar.gz" ]
+then
+	wget -P $TMPDIR http://sourceforge.net/projects/soci/files/soci/soci-3.2.2/soci-3.2.2.tar.gz
+fi
+
+# Check for soci source
+if [ ! -d "$BASEDIR/soci-3.2.2" ]
+then
+	tar xvfz $TMPDIR/soci-3.2.2.tar.gz -C $BASEDIR
+fi
+
+# Make cmake
 cd $BASEDIR/cmake-2.8.12.2
-./bootstrap
+if [ ! -d "$BASEDIR/cmake-2.8.12.2/Bootstrap.cmk" ]
+then
+	./bootstrap
+fi
 make
+
+# Make soci
 cd $BASEDIR/soci-3.2.2
 mkdir build
 cd build
@@ -12,5 +50,19 @@ export PATH=$PATH:$BASEDIR/cmake-2.8.12.2/bin
 cmake -G "Unix Makefiles" -DWITH_BOOST=OFF ../
 make
 cd ../../
+
+# Create output directories
+if [ ! -d "$BASEDIR/bin" ]
+then
+	mkdir $BASEDIR/bin
+fi
+if [ ! -d "$BASEDIR/obj" ]
+then
+	mkdir $BASEDIR/obj
+fi
+
+# Make modeler
 make
+
+# Return to root directory
 cd ../
